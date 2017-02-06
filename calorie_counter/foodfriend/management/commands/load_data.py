215fb7b9a916d4726b8661600c491a9b0908cd77
Tomasz Tomasik <tomasz.szymon.tomasik@gmail.com>
2017-02-06 18:42:42 +0100

    updated load_data and searched for bugs

--- calorie_counter/foodfriend/management/commands/load_data.py
+++ calorie_counter/foodfriend/management/commands/load_data.py
@@ -39,6 +39,7 @@ def populate():
     Food.objects.update_or_create(name="Szynka Parmeńska Prosciutto di Parma", kcal=273, proteins=27, carbs=0.7, fats=18.6, grams=100)
     Food.objects.update_or_create(name="Papryka", kcal=32, proteins=1.7, carbs=4, fats=0.5, grams=100)
     Food.objects.update_or_create(name="Ser Cheddar", kcal=379, proteins=25, carbs=0, fats=31, grams=100)
+    Food.objects.update_or_create(name="Bataty", kcal=86, proteins=1.6, carbs=20.1, fats=0.1, grams=100)
 
 class Command(BaseCommand):
     help = 'Initialize database'
